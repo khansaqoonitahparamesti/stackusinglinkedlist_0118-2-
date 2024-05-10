a4939a0e55465e4c8f80e06b60b9caabaa66db69
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-05-10 12:01:04 +0700

    mengimplementasikan pop di swtich case dangan menggunakn is empty

--- stackusinglinkedlist_0118/stackusinglinkedlist_0118.cpp
+++ stackusinglinkedlist_0118/stackusinglinkedlist_0118.cpp
@@ -85,6 +85,14 @@ int main() {
 			cin >> value;
 			stack.push(value);
 			break;
+		case 2:
+			if (!stack.isEmpty()) {
+				stack.pop();
+			}
+			else {
+				cout << "stack is empty. cannot pop." << endl;
+			}
+			break;
 		}
 	}
 }
