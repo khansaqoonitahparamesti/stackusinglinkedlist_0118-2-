b1cb584177ab60b1820bab81048eecfa9c474348
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-05-10 10:36:12 +0700

    Add project files.

---
+++ stackusinglinkedlist_0118/stackusinglinkedlist_0118.cpp
@@ -0,0 +1,9 @@
+// stackusinglinkedlist_0118.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+// node class representing a single node in the linked list
+class Node {
+};
